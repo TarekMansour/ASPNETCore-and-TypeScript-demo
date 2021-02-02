fafdb622a590ef83fed3585ac7e3bc3229aad49a
Tarek MANSOUR <tmansour@mantu.com>
2021-02-02 18:49:18 +0100

    set up gulp

---
+++ App.web/gulpfile.js
@@ -0,0 +1,18 @@
+﻿//This file is the main entry point for defining Gulp tasks and using Gulp plugins
+
+var gulp = require("gulp");
+var del = require("del");
+
+var paths = {
+    scripts: ["scripts/**/*.js", "scripts/**/*.ts", "scripts/**/*.map"],
+};
+
+// run the ‘clean’ task when you ask Visual Studio to clean the build
+gulp.task("clean", function () {
+    return del(["wwwroot/scripts/**/*"]);
+});
+
+//run the task ‘default’ after the build finishes
+gulp.task("default", function () {
+    gulp.src(paths.scripts).pipe(gulp.dest("wwwroot/scripts"));
+});
