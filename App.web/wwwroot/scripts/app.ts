28b9724c2d96da6ae2fdafbc77af74c8e4c1368a
Tarek MANSOUR <tmansour@mantu.com>
2021-02-02 19:00:32 +0100

    add html file and start task runner to generate the needed files

--- App.web/wwwroot/scripts/app.ts
+++ App.web/wwwroot/scripts/app.ts
@@ -1,9 +1,8 @@
 //replace the default hello message with a typeScript function
 function sayHello() {
-    const compiler = document.getElementById("compiler")
+    const compiler = (document.getElementById("compiler") as HTMLInputElement)
         .value;
-    const framework = document.getElementById("framework")
+    const framework = (document.getElementById("framework") as HTMLInputElement)
         .value;
     return `Hello from ${compiler} and ${framework}!`;
 }
-//# sourceMappingURL=app.js.map
